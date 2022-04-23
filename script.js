5a5b60f8062d4b18a7108344f4f36f1301430063
kaderkaeva <89255147+kaderkaeva@users.noreply.github.com>
2022-04-23 19:43:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,69 @@
+const goods = [
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 },
+];
+
+const GET_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/catalogData.json'
+const GET_CART_GOODS_ITEMS = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/getBasket.json'
+
+function service(url, callback) {
+  xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send();
+  xhr.onload = () => {
+    callback(JSON.parse(xhr.response))
+  }
+}
+
+class GoodsItem {
+  constructor({ product_name, price }) {
+    this.product_name = product_name;
+    this.price = price;
+  }
+  render() {
+    return `
+    <div class="goods-item">
+      <h3>${this.product_name}</h3>
+      <p>${this.price}</p>
+    </div>
+  `;
+  }
+}
+class GoodsList {
+  items = [];
+  fetchGoods(callback) {
+    service(GET_GOODS_ITEMS, (data) => {
+      this.items = data;
+      callback()
+    });
+  }
+  calculatePrice() {
+    return this.items.reduce((prev, { price }) => {
+      return prev + price;
+    }, 0)
+  }
+  render() {
+    const goods = this.items.map(item => {
+      const goodItem = new GoodsItem(item);
+      return goodItem.render()
+    }).join('');
+
+    document.querySelector('.goods-list').innerHTML = goods;
+  }
+}
+class GoodsCart {
+  items = [];
+  fetchGoods() {
+    service(GET_CART_GOODS_ITEMS, (data) => {
+      this.items = data;
+      callback()
+    });
+  }
+}
+
+const goodsList = new GoodsList();
+goodsList.fetchGoods(() => {
+  goodsList.render();
+});
